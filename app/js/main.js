0bda2d075e8cfc2bb34c443a7c3bcd4a6b8b5a67
Bojan Kolano <bojan19941@hotmail.com>
2019-12-19 23:02:27 +0100

    finished header and started hero

--- app/js/main.js
+++ app/js/main.js
@@ -2,6 +2,38 @@
 
 $('document').ready(function(){
 
+  var hamburgerIcon = $('.hamburger__javascript');
+  var dropNav = $('.header__nav');
+  var headerDropIcon = $('.header__drop__icon');
+  
+  hamburgerIcon.on('click', function(){
     
+    dropNav.toggleClass('nav__show');
+    $(this).toggleClass('transform');
+
+    if ($(this).hasClass('transform')) {
+      headerDropIcon.removeClass('s-hamburger');
+      headerDropIcon.addClass('s-close');
+    }
+
+    else {
+      headerDropIcon.addClass('s-hamburger');
+      headerDropIcon.removeClass('s-close');
+    }
+  })
+
+  $(window).scroll(function() {
+
+    var header = $('.header__javascript');
+    var scroll = $(window).scrollTop();
+
+    if (scroll > 0) {
+      header.addClass('active');
+    }
+
+    else {
+      header.removeClass('active');
+    }
+  })
    
 });
